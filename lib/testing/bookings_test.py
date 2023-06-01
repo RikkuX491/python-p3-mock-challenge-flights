3285de51bb7232ed546369d1a3747209772316dd
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-01 10:22:34 -0400

    Update bookings_test.py

--- lib/testing/bookings_test.py
+++ lib/testing/bookings_test.py
@@ -1,44 +1,95 @@
+import pytest
+
 from classes.flight import Flight
 from classes.customer import Customer
 from classes.booking import Booking
-import pytest
 
-class TestBookings:
-    '''Booking in booking.py'''
+class TestCustomer:
+    '''Customer in customer.py'''
 
-    def test_has_price(self):
-        '''has the price passed into __init__'''
-        flight = Flight("Alaska Airlines")
-        customer = Customer('Tom', 'Jackson')
-        booking_1 = Booking(customer, flight, 524)
-        booking_2 = Booking(customer, flight, 2354)
+    def test_has_name(self):
+        '''has the first name and last name passed into __init__'''
+        customer = Customer('Alex', 'Smith')
+        assert customer.first_name == "Alex"
+        assert customer.last_name == "Smith"
 
-        assert(booking_1.price == 524)
-        assert(booking_2.price == 2354)
+    def test_validates_name(self):
+        '''has first and last names as strings between 1 and 25 characters, inclusive'''
         with pytest.raises(Exception):
-            booking_1.price = 6000
+            Customer('', 'Johnson')
+
         with pytest.raises(Exception):
-            booking_1.price = "2000"
+            Customer('Bruce', '')
+
         with pytest.raises(Exception):
-            booking_1.price = -300
+            Customer('B' * 26, 'Johnson')
 
-    def test_has_a_customer(self):
-        '''booking has a customer'''
-        flight = Flight("Jetblue")
-        customer = Customer('Steve', 'Jobs')
-        booking_1 = Booking(customer, flight, 2500)
-        booking_2 = Booking(customer, flight, 655)
+        with pytest.raises(Exception):
+            Customer('Bruce', 'J' * 26)
 
-        assert(booking_1.customer == customer)
-        assert(booking_2.customer == customer)
+        with pytest.raises(Exception):
+            Customer(1, 'Johnson')
 
-    def test_has_a_flight(self):
-        '''Booking has a flight.'''
+        with pytest.raises(Exception):
+            Customer('Bruce', 2)
+
+    def test_has_many_bookings(self):
+        '''customer has many bookings'''
         flight = Flight("American Airlines")
-        customer = Customer('Steve', 'Jobs')
-        customer_2 = Customer('Jack', 'Smith')
-        booking_1 = Booking(customer, flight, 2500)
-        booking_2 = Booking(customer_2, flight, 1500)
+        customer = Customer('Chris', 'Jackson')
+        booking_1 = Booking(customer, flight, 2000)
+        booking_2 = Booking(customer, flight, 2500)
 
-        assert(booking_1.flight == flight)
-        assert(booking_2.flight == flight)
+        assert len(customer.bookings()) == 2
+        assert booking_1 in customer.bookings()
+        assert booking_2 in customer.bookings()
+
+    def test_bookings_must_be_booking_type(self):
+        '''customer bookings are of type Booking'''
+        customer = Customer("David", "Stevenson")
+        customer.bookings(1)
+        assert not customer.bookings()
+        flight = Flight("Jetblue")
+        booking = Booking(customer, flight, 1550)
+        customer.bookings(booking)
+        assert booking in customer.bookings()
+
+    def test_has_many_flights(self):
+        '''customer has many flights.'''
+        flight = Flight("Hawaiian Airlines")
+        flight_2 = Flight("Delta Air Lines")
+
+        customer = Customer('Ernie', 'Thompson')
+        Booking(customer, flight, 2500)
+        Booking(customer, flight_2, 1500)
+
+        assert flight in customer.flights()
+        assert flight_2 in customer.flights()
+
+    def test_flights_must_be_flight_type(self):
+        '''customer flights are of type Flight'''
+        customer = Customer("Frank", "Davidson")
+        customer.flights(1)
+        assert not customer.flights()
+        flight = Flight("Alaskan Airlines")
+        customer.flights(flight)
+        assert flight in customer.flights()
+
+    def test_customer_flights_unique(self):
+        '''customer flights are unique'''
+        customer = Customer("Greg", "Stevenson")
+        flight = Flight("American Airlines")
+        customer.flights(flight)
+        assert flight in customer.flights() and len(customer.flights()) == 1
+        customer.flights(flight)
+        assert len(customer.flights()) == 1
+
+
+    def test_get_number_of_bookings(self):
+        '''test get_number_of_bookings()'''
+        flight = Flight("Spirit Airlines")
+        customer = Customer('Steve', 'Jobs')
+        Booking(customer, flight, 2500)
+        Booking(customer, flight, 1000)
+
+        assert customer.num_bookings() == 2

f7cf3503c1ceea553c6222a5d43165b95c1fe32a
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-01 10:22:17 -0400

    Update bookings_test.py

--- lib/testing/bookings_test.py
+++ lib/testing/bookings_test.py
@@ -10,24 +10,24 @@ class TestBookings:
         '''has the price passed into __init__'''
         flight = Flight("Alaska Airlines")
         customer = Customer('Tom', 'Jackson')
-        booking_1 = Booking(customer, flight, 2)
-        booking_2 = Booking(customer, flight, 5)
+        booking_1 = Booking(customer, flight, 524)
+        booking_2 = Booking(customer, flight, 2354)
 
-        assert(booking_1.price == 2)
-        assert(booking_2.price == 5)
+        assert(booking_1.price == 524)
+        assert(booking_2.price == 2354)
         with pytest.raises(Exception):
-            booking_1.price = 6
+            booking_1.price = 6000
         with pytest.raises(Exception):
-            booking_1.price = "6"
+            booking_1.price = "2000"
         with pytest.raises(Exception):
-            booking_1.price = -3
+            booking_1.price = -300
 
     def test_has_a_customer(self):
         '''booking has a customer'''
         flight = Flight("Jetblue")
         customer = Customer('Steve', 'Jobs')
-        booking_1 = Booking(customer, flight, 2)
-        booking_2 = Booking(customer, flight, 5)
+        booking_1 = Booking(customer, flight, 2500)
+        booking_2 = Booking(customer, flight, 655)
 
         assert(booking_1.customer == customer)
         assert(booking_2.customer == customer)
@@ -37,8 +37,8 @@ class TestBookings:
         flight = Flight("American Airlines")
         customer = Customer('Steve', 'Jobs')
         customer_2 = Customer('Jack', 'Smith')
-        booking_1 = Booking(customer, flight, 2)
-        booking_2 = Booking(customer_2, flight, 5)
+        booking_1 = Booking(customer, flight, 2500)
+        booking_2 = Booking(customer_2, flight, 1500)
 
         assert(booking_1.flight == flight)
         assert(booking_2.flight == flight)

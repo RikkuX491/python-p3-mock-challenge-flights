2b6001d0d71815041db8745ee2ed8229909eafc4
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-01 10:21:52 -0400

    Update booking_test.py

--- lib/testing/booking_test.py
+++ lib/testing/booking_test.py
@@ -11,25 +11,25 @@ class TestBookings:
         '''has the customer, flight, and price passed into __init__'''
         customer = Customer('Steve', 'Jobs')
         flight = Flight("Mels")
-        booking = Booking(customer, flight, 2)
+        booking = Booking(customer, flight, 2500)
 
         assert booking.customer == customer
         assert booking.flight == flight
-        assert booking.price == 2
+        assert booking.price == 2500
 
     def test_validates_customer(self):
         '''checks to ensure customer is of type Customer'''
         flight = Flight("Delta Air Lines")
         
         with pytest.raises(Exception):
-            Booking(1, flight, 2)
+            Booking(1, flight, 2300)
 
     def test_validates_flight(self):
         '''checks to ensure flight is of type Flight'''
         customer = Customer('Rick', 'Jackson')
         
         with pytest.raises(Exception):
-            Booking(customer, 1, 2)
+            Booking(customer, 1235, 2300)
 
     def test_validates_price(self):
         '''checks to ensure price is integer between 1 and 5, inclusive'''
@@ -37,7 +37,7 @@ class TestBookings:
         flight = Flight("Jetblue")
 
         with pytest.raises(Exception):
-            Booking(customer, flight, 0)
+            Booking(customer, flight, 450)
 
         with pytest.raises(Exception):
-            Booking(customer, flight, 6)
+            Booking(customer, flight, 4000)
